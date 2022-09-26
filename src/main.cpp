8df0c13ee785dcc375b970992a0ff8891ab05b09
Rich Bayliss <richbayliss@gmail.com>
2022-09-26 10:35:16 +0100

    ✨: Added Arduino Uno support
    Change-type: patch
    Signed-off-by: Rich Bayliss <richbayliss@gmail.com>

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,24 @@
-#include <Arduino.h>`
+#include <Arduino.h>
+
+#include "../lib/mcp4xxx/mcp4xxx.h"
+
+using namespace icecave::arduino;
+
+MCP4XXX *digipot;
 
 void setup() {
-  // put your setup code here, to run once:
+  digipot = new MCP4XXX();
+  digipot->set(0);
+
+  Serial.begin(9600);
+  Serial.println("ESP32 Rolec Solar Diverter");
+  Serial.println(digipot->max_value(), 10);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  for (int pos = 0; pos < digipot->max_value(); pos++) {
+    digipot->set(pos);
+    Serial.println(pos);
+    delay(100);
+  }
 }
